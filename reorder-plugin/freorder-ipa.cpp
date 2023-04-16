c60b6ce724205d744516f778975833712be2c5ce
Dmitry Nechitaev <nechda6@gmail.com>
2023-04-16 22:24:02 +0300

    Adapt plugin for gcc-12

--- reorder-plugin/freorder-ipa.cpp
+++ reorder-plugin/freorder-ipa.cpp
@@ -69,7 +69,7 @@ static unsigned int reorder_functions() {
 
     cgraph_node *node;
     FOR_EACH_DEFINED_FUNCTION(node) {
-        if (node == nullptr && !node->alias && !node->global.inlined_to)
+        if (node == nullptr && !node->alias && !node->inlined_to)
             continue;
 
         int id = 0;
